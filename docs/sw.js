4e2a0a8e8995ecb78f5372d388054b40de1eaa40
Sandeep Rathour <deep_rathour@yahoo.com>
2025-04-22 23:45:24 +1000

    Update sw.js

--- docs/sw.js
+++ docs/sw.js
@@ -1,25 +1,27 @@
-const CACHE_NAME = 'focus-timer-cache-v1';
-const ASSETS = [
-  '/',
-  '/index.html',
-  '/popup.css',
-  '/popup.js',
-  '/manifest.webmanifest',
-  '/icons/icon128.png',
-  '/sounds/rain.mp3',
-  '/sounds/coffee.mp3',
-  '/sounds/white.mp3',
-  '/sounds/notification.mp3'
+const CACHE = 'focus-timer-v1';
+const assets = [
+  './',
+  './index.html',
+  './popup.css',
+  './popup.js',
+  './manifest.webmanifest',
+  './icons/icon16.png',
+  './icons/icon48.png',
+  './icons/icon128.png',
+  './sounds/rain.mp3',
+  './sounds/coffee.mp3',
+  './sounds/white.mp3',
+  './sounds/notification.mp3'
 ];
 
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
-  );
-});
+self.addEventListener('install', evt =>
+  evt.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(assets))
+  )
+);
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(cached => cached || fetch(event.request))
-  );
-});
+self.addEventListener('fetch', evt =>
+  evt.respondWith(
+    caches.match(evt.request).then(res => res || fetch(evt.request))
+  )
+);
